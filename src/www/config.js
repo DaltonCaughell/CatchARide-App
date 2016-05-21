0d81ae86ac02ce0bb553e9538b9243abaf1d949b
dalton <daltoncaughell@gmail.com>
2016-05-20 23:01:49 -0700

    Fonts n' shit

--- src/www/config.js
+++ src/www/config.js
@@ -1,1 +1,19 @@
+var Config = {
+    API: {
+        Protocol: "http://",
+        Host: "localhost:3000",
+        Version: "/api/v1",
+        Base: ""
+    },
+    DOM: {
+        LoadingOverlay: 'loadingOverlay'
+    }
+};
 
+Config.API.Base = Config.API.Protocol + Config.API.Host + Config.API.Version;
+
+Config.API.Endpoints = {
+    User: {
+        GetProductJSON: Config.API.Base + "/user/me",
+    }
+};
