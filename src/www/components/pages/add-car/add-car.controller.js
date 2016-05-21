aac87d3ecab4b9421424db90e83770ebc29f94bc
dalton <daltoncaughell@gmail.com>
2016-05-21 16:26:22 -0700

    Adding a user service and fixing addcar

--- src/www/components/pages/add-car/add-car.controller.js
+++ src/www/components/pages/add-car/add-car.controller.js
@@ -1,9 +1,13 @@
-mainApp.controller("AddCarController", function($scope, $http, $location, $state, Auth, crLoading) {
+mainApp.controller("AddCarController", function($scope, $http, $location, $state, Auth, crLoading, crUser) {
 
     $scope.errors = {};
 
-    $scope.add = function() {
+    $scope.add = function(dlNumber, brand, model, seats, licensePlateNumber) {
+        crUser.addCar(dlNumber, brand, model, seats, licensePlateNumber).then(function(data) {
 
+        }, function(error) {
+
+        });
     };
 
     $scope.skip = function() {
---
+++ src/www/components/services/user.service.js
@@ -0,0 +1,11 @@
+mainApp.factory('crUser', function($http) {
+
+    var service = {};
+
+    service.addCar = function(dlNumber, brand, model, seats, licensePlateNumber) {
+
+    };
+
+    return service;
+
+});
